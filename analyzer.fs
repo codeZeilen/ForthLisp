147e21e6f373505d4189cc0232e40b1c43f93e73
Patrick R <patrick.rein@gmail.com>
2013-10-21 12:00:02 +0200

    added analysis of literal elements

--- analyzer.fs
+++ analyzer.fs
@@ -5,21 +5,43 @@ s" ./datatypes.fs" included
     over dup >string-length 1 - swap >string-at 39 =
     and ;
 
+: >string-from-sc-string ( a -- a )
+    dup >string-content 1 chars +
+    over >string-length 2 - dup >r r@ chars allocate throw >r r@ \ Get memory
+    swap cmove 
+    r> r> make-string ;
+
 : analyze-string? ( a -- b ) ( exp -- b )
    >string-sc-string? ; 
+
+: o-analyze-string? ( a -- a b )
+    dup analyze-string? ;
+
+: _string-execution ( a a -- a ) ( evt string -- string )
+    swap drop ;
+
+: analyze-string ( a -- a ) ( string -- 1call )
+    >string-from-sc-string ['] _string-execution make-1call ;
 
 : analyze-number? ( a -- b ) ( exp -- b )
     >string-numeric? ;
 
-: analyze-number
-    ;
+: o-analyze-number? ( a -- a b )
+    dup analyze-number? ;
+
+: _number-execution ( a a -- a ) ( evt number -- number )
+    swap drop ;
+
+: analyze-number ( a -- a ) ( string -- 1call )
+    >string-to-number ['] _number-execution make-1call ;
 
 : _sc-analyze ( a -- a ) ( parsed-list -- analyzed-tree )
     o-analyze-string? if
-
+        analyze-string
     else o-analyze-number? if
-
+        analyze-number
     endif
     endif ;
 
 : sc-analyze ( a -- a ) ( parsed-list -- analyzed tree )
+    ;
--- tests-analyzer.fs
+++ tests-analyzer.fs
@@ -2,7 +2,11 @@ s" ./testing.fs" included
 s" ./analyzer.fs" included
 
 s" 'hallo'" make-string
->string-sc-string? assert
+    >string-sc-string? assert
+
+s" 'hallo'" make-string
+>string-from-sc-string >string-length 5
+    = assert
 
 s" 20" make-string analyze-number?
     assert
@@ -12,3 +16,17 @@ s" 'hallo'" make-string analyze-string?
 
 s" ''" make-string analyze-string?
     assert
+
+s" 20" make-string _sc-analyze 0 swap call-execute >number-value 20 
+    = assert
+
+s" 0" make-string _sc-analyze 0 swap call-execute >number-value 0 
+    = assert
+
+s" 100000" make-string _sc-analyze 0 swap call-execute >number-value 100000 
+    = assert
+
+s" 'hallo'" make-string _sc-analyze 
+0 swap call-execute
+>string-length 5
+    = assert
